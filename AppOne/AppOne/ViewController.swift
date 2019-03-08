a916b64c348e49e32c269a3eef8bb3edd2fbdecd
Luis Mauricio Esparza Vázquez <schockarum.9503@gmail.com>
2019-03-07 20:51:36 -0600

    AppOne & otros archivos

---
+++ AppOne/AppOne/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AppOne
+//
+//  Created by Luis Mauricio Esparza Vazquez on 2/2/19.
+//  Copyright © 2019 Schock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .red
+    }
+
+
+}
+
