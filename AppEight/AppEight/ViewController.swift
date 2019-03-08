65c73c818eb938186317354fe1778cedccf54642
Luis Mauricio Esparza Vázquez <schockarum.9503@gmail.com>
2019-03-08 16:59:25 -0600

    AppEight concluye temporalmente, se agregó el segundo botón y acciones comentadas

--- AppEight/AppEight/ViewController.swift
+++ AppEight/AppEight/ViewController.swift
@@ -20,7 +20,20 @@ class ViewController: UIViewController {
         
         self.present(alert, animated: true, completion: nil) //Con esto presentamos la alerta animada cuando presionemos el boton.
         
-        alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: { (action) in print ("presionamos el boton cancelar")})) // Se agrega la opcion cancelar, que cancela la alerta cuando sale la accion en pantalla, cuando cancelamos, el hanler imprime en consola que se presionó el boton cancelar
+        alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: { (action) in
+            print ("presionamos el boton cancelar")
+            self.view.backgroundColor = UIColor.white
+        })) // Se agrega la opcion cancelar, que cancela la alerta cuando sale la accion en pantalla, cuando cancelamos, el hanler imprime en consola que se presionó el boton cancelar. Si hacemos al handler "nil", no imprime nada, duh.
+        // Como extra, este boton regresa al fondo blanco
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: {(action) in
+            self.view.backgroundColor = UIColor.orange
+        })) //Agregamos el otro botón a la alerta, que cambia el fondo cuando se pica
+        
+        self.view.backgroundColor = .black /*Esto sucede fuera de la alerta. El procedimiento es:
+         1) Sucede el click a la alerta y se carga todo
+         2) Se cambia el fondo a negro porque la alerta está cargada en memoria
+         3) Al seleccionar una opción, el fondo vuelve a cambiar de color*/
     }
     
 }
