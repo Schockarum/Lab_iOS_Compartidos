d95982fbec0b372d5406c252fec5c74750061a1c
Luis Mauricio Esparza Vázquez <schockarum.9503@gmail.com>
2019-03-08 16:52:36 -0600

    Se agrega el boton cancelar a la alerta; imprime en consola al cancelar

--- AppEight/AppEight/ViewController.swift
+++ AppEight/AppEight/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
         let alert = UIAlertController(title: "Alert", message: "Las alertas pueden aparecer en cualquier momento", preferredStyle: .alert) //Declaramos una alerta con titulo, mensaje y accion nula de completitud
         
         self.present(alert, animated: true, completion: nil) //Con esto presentamos la alerta animada cuando presionemos el boton.
+        
+        alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: { (action) in print ("presionamos el boton cancelar")})) // Se agrega la opcion cancelar, que cancela la alerta cuando sale la accion en pantalla, cuando cancelamos, el hanler imprime en consola que se presionó el boton cancelar
     }
     
 }
