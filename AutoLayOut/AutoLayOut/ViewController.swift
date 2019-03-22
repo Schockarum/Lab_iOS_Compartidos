f49552a2dee3d8fa928bebfca1b87c5f6509cfb1
Luis Mauricio Esparza Vázquez <schockarum.9503@gmail.com>
2019-03-22 17:38:03 -0600

    AutoLayOut

---
+++ AutoLayOut/AutoLayOut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayOut
+//
+//  Created by Luis Mauricio Esparza Vazquez on 3/22/19.
+//  Copyright © 2019 Schock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
