c1c6a722993c96a6de191677c4e3740fb3759a7b
Luis Mauricio Esparza Vázquez <schockarum.9503@gmail.com>
2019-03-23 11:36:32 -0600

    Se arregla el giro, pero quitamos navigation. Hacerlo en el branch CVController

---
+++ UICollectionView/UICollectionView/MySupplementaryView.swift
@@ -0,0 +1,13 @@
+//
+//  MySupplementaryView.swift
+//  UICollectionView
+//
+//  Created by Luis Mauricio Esparza Vazquez on 3/23/19.
+//  Copyright © 2019 Schock. All rights reserved.
+//
+
+import UIKit
+
+class MySupplementaryView: UICollectionReusableView {
+        
+}
