3991a24d6b7d15f1383fbcc17a397354a2a4995c
Luis Mauricio Esparza Vázquez <schockarum.9503@gmail.com>
2019-09-11 09:07:15 -0500

    Archivos finales, diplomado terminado

---
+++ HolaMundo/AppOne/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  AppOne
+//
+//  Created by Luis Mauricio Esparza Vazquez on 2/2/19.
+//  Copyright © 2019 Schock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .red
+        
+        let var1 = 2
+        let var2 = 3
+        if var1 == var2{
+            let var3 = true
+        } else {
+            let var3 = false
+        }
+    }
+
+
+}
+
---
+++ UIFont.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var a: Int
+if let _ = UIFont(name: "AvenirNext", size: 30)  {
+    a = 1
+} else {
+    a = 2
+}
